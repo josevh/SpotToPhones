b0fe1647f41e816144e47a0c5194846801a21173
overthinkingme <josevictorherrera@gmail.com>
2015-04-25 19:53:03 -0700

    fixed auth problem, getSpotTracks()
    Fixed auth problem on getSpotTracks().
    It now returns all tracks in the playlist in a 2D list.
    track_data[x][x]

--- SpotToPhones.py
+++ SpotToPhones.py
@@ -1,5 +1,3 @@
-## Work in progress
-
 from __future__ import absolute_import
 from __future__ import print_function
 import ssl
@@ -32,59 +30,63 @@ def ConfigSectionMap(section):
 def getSpotTracks():
     app_scope = ConfigSectionMap("SPOTIPY")['scope']
     username = ConfigSectionMap("SPOTIPY")['user']
-    token = util.prompt_for_user_token(username,
-        scope=app_scope,
+    token = util.prompt_for_user_token(username, scope=app_scope,
         client_id=ConfigSectionMap("SPOTIPY")['spotipy_client_id'],
         client_secret=ConfigSectionMap("SPOTIPY")['spotipy_client_secret'],
         redirect_uri=ConfigSectionMap("SPOTIPY")['spotipy_redirect_uri'])
+
     if token:
         sp = spotipy.Spotify(auth=token)
         playlists = sp.user_playlists(username)
-        pp.pprint(playlists)
     else:
         print("Can't get token for " + username)
         sys.exit()
 
     playlist_name = ConfigSectionMap("General")['playlist_name']
-    track_data = []
-    track_data.append([])
-    track_data.append([])
 
     playlists = sp.user_playlists(username)
 
+    playlist_found_test = 0
     for playlist in playlists['items']:
-        print("x_ playlistname: " + playlist_name)
         if playlist['name'] == playlist_name:
-            print("x_ playlist name matches\n")
             tracks = sp.user_playlist(username, playlist['id'], fields="tracks")
-        else:
-            print("Unable to find playlist.")
-            sys.exit()
+            playlist_found_test = 1
+            break
+
+    if playlist_found_test == 0:
+        print("Did not find playlist.")
+        sys.exit()
+
+    track_data = []
 
     x = 0
     for track in tracks['tracks']['items']:
+        track_data.append([])           #adds a new row in 2d array/list
+        #pp.pprint(track['track'])        
         #artist, album, track
-        track_data[x].append(track['track']['artists'][0]['name'])
-        track_data[x].append(track['track']['album']['name'])
-        track_data[x].append(track['track']['name'])
-        # track_album = track['track']['album']['name']
-        # track_artist = track['track']['artists'][0]['name']
-        # track_name = track['track']['name']
+        track_artist = track['track']['artists'][0]['name']
+        track_album = track['track']['album']['name']
+        track_name = track['track']['name']
+        track_data[x].append(track_artist)
+        track_data[x].append(track_album)
+        track_data[x].append(track_name)
         x+=1
-    print(track_data[0][0])
-    print(track_data[0][1])
-    print(track_data[0][2])
+    
+    return track_data
+    
+def main():
+    track_data = getSpotTracks()
+    
+    ### TESTING ###
+    print(track_data[0][0]) #[0] artist
+    print(track_data[0][1]) #[0] album
+    print(track_data[0][2]) #[0] track
     print(track_data[1][0])
     print(track_data[1][1])
     print(track_data[1][2])
     print(track_data[2][0])
     print(track_data[2][1])
     print(track_data[2][2])
-    print("end of getSpotTracks")
-
-def main():
-    getSpotTracks()
 
 
 main()
-##########testing#############################################################
