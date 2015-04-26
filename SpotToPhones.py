027598e9bcfd8ce0f2c4fba4e47a184f2ea2deb0
overthinkingme <josevictorherrera@gmail.com>
2015-04-25 23:36:32 -0700

    Added the start of Headphones code

--- SpotToPhones.py
+++ SpotToPhones.py
@@ -74,8 +74,76 @@ def getSpotTracks():
 
     return track_data
 
+def callHeadphones(cmd):
+    hp_user = ConfigSectionMap("HEADPHONES")['user']
+    hp_pass = ConfigSectionMap("HEADPHONES")['password']
+    hp_ip = ConfigSectionMap("HEADPHONES")['ip']
+    # hp_root = ""  #web_root not implemented
+    hp_port = ConfigSectionMap("HEADPHONES")['port']
+    hp_api_key = ConfigSectionMap("HEADPHONES")['api_key']
+    hp_api_url = "http://" + hp_ip + ":" + hp_port + "/api?apikey=" + hp_api_key + "&cmd="
+    request = hp_api + cmd
+    result = requests.get(request)
+    result = result.json()
+    return result
+
+def checkHeadphones(track_data):
+    #search for tracks/albums/artists in Headphones libray
+    hp_index = callHeadphones('getIndex')
+    for x in range(0,len(track_data)):
+        spArtist = track_data[x][0]
+        spAlbum = track_data[x][1]
+        spTrack = track_data[x][2]
+        for x in range(0,len(hp_index)):
+            if hp_index[x]['ArtistName'] == spArtist:
+                hp_artist_id = hp_index[x]['ArtistID']
+            else:
+                hp_artist_id = "notfound"
+                #need to query musicbrainz for artistid
+                #and query for the album id and add to wanted
+                #and end this function, do not search for
+                #albums and tracks since artist is not in library
+                break
+        #search for albums in Headphones library
+        hp_query = 'getArtist&id=' + hp_artist_id
+        hp_albums = callHeadphones(hp_query)
+        for x in range(0,len(hp_albums)):
+            if hp_albums['albums'][x]['AlbumTitle'] == spAlbum:
+                hp_album_id = hp_albums['albums'][x]['AlbumID']
+            else:
+                hp_album_id = "notfound"
+                #do stuff
+                break
+        #search for track if Album in library
+        hp_query = 'getAlbum&id=' + hp_track_album_id
+        hp_tracks = callHeadphones(hp_query)
+        for x in range(0,len(hp_tracks)):
+            if hp_tracks['tracks'][x]['TrackTitle'] == spTrack:
+                #have the artist, album, date
+                #do not queue, remove from spotify playlist
+                break
+            else:
+                #have artist and incomplete album
+                #redownload? investigate further
+                #could be an issue in deluxe album vs normal
+                break
+
+
+    #return an array with artistid and albumid to add to headphones
+
+def queueAlbum(artist_id, album_id):    #in headphones
+    print("todo")
+def getMusicbrainzArtistID():
+    print("todo")
+    #findArtist&name=$artistname[&limit=$limit]
+    #returns artist id
+def getMusicbrainzAlbumID():
+    print("todo")
+
+
 def main():
     track_data = getSpotTracks()
+    checkHeadphones(track_data)
 
     ### TESTING ###
     print(track_data[0][0]) #[0] artist
@@ -87,6 +155,5 @@ def main():
     print(track_data[2][0])
     print(track_data[2][1])
     print(track_data[2][2])
-
 
 main()
