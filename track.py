60728f40fcc557e1363af31ea2eee8d7a333035b
Jose Herrera <josevictorherrera@gmail.com>
2015-09-08 03:55:58 +0000

    initialize class variables

--- track.py
+++ track.py
@@ -12,7 +12,7 @@ class Track(object):
         
         mb_id:              A string representing the track's Musicbrainz ID.
         mb_album_id:        A string representing the track's Album's Musicbrainz ID.
-        mb_album_id:        A string representing the track's Artist's Musicbrainz ID.
+        mb_artist_id:        A string representing the track's Artist's Musicbrainz ID.
         
         have_track:         A boolean representing whether the track is in Headphones library.
         have_album:         A boolean representing whether the track's album is in Headphones library.
@@ -21,14 +21,23 @@ class Track(object):
     """
 
     def __init__(self, name, album, artist, uri, sp_uri, sp_id, sp_album_id, sp_artist_id):
-        self.name           = name
-        self.album          = album
-        self.artist         = artist
+        self.name               = name
+        self.album              = album
+        self.artist             = artist
         
-        self.sp_uri         = sp_uri
-        self.sp_id          = sp_id
-        self.sp_album_id    = sp_album_id
-        self.sp_artist_id   = sp_artist_id
+        self.sp_uri             = sp_uri
+        self.sp_id              = sp_id
+        self.sp_album_id        = sp_album_id
+        self.sp_artist_id       = sp_artist_id
+        
+        self.mb_id              = ""
+        self.mb_album_id        = ""
+        self.mb_artist_id       = ""
+        
+        self.have_track         = False
+        self.have_album         = False
+        
+        self.dl_request_status  = ""
         
     def getMB_id(self, spotify_id):
         # necessary?
