36f7409c90e087c39b8d0297238f4d213733ef71
Jose Herrera <josevictorherrera@gmail.com>
2015-09-11 15:59:46 +0000

    revert method movements

--- track.py
+++ track.py
@@ -1,5 +1,3 @@
-import pyen
-
 class Track(object):
     """Tracks have the following attributes:
     
@@ -14,7 +12,7 @@ class Track(object):
         
         mb_id:              A string representing the track's Musicbrainz ID.
         mb_album_id:        A string representing the track's Album's Musicbrainz ID.
-        mb_artist_id:        A string representing the track's Artist's Musicbrainz ID.
+        mb_artist_id:       A string representing the track's Artist's Musicbrainz ID.
         
         have_track:         A boolean representing whether the track is in Headphones library.
         have_album:         A boolean representing whether the track's album is in Headphones library.
@@ -32,63 +30,11 @@ class Track(object):
         self.sp_album_id        = sp_album_id
         self.sp_artist_id       = sp_artist_id
         
-        self.mb_id              = self.__getMB_id(sp_id)
-        self.mb_album_id        = self.__getMB_album_id(mb_artist_id, album)
-        self.mb_artist_id       = self.__getMB_artist_id(sp_id)
+        self.mb_id              = None
+        self.mb_album_id        = None
+        self.mb_artist_id       = None
         
-        self.have_track         = False
-        self.have_album         = False
+        self.have_track         = None
+        self.have_album         = None
         
-        self.dl_request_status  = ""
-        
-    def __getMB_id(self, sp_id):
-        # necessary?
-        
-    def __getMB_artist_id(self, sp_id):
-        ''' Use EchoNest API to map Spotify artist id to Musicbrainz artist id
-        '''
-        en = pyen.Pyen(ConfigSectionMap("ECHONEST")['api_key'])
-        params = {
-            'id':       'spotify:artist:' + artistSPID,
-            'bucket':   ['id:musicbrainz'],
-        }
-        response = en.get('artist/profile', **params)
-        if (response['status']['message'] == 'Success'):
-            mbid = response['artist']['foreign_ids'][0]['foreign_id']
-            return mbid[19:]    #remove 'musicbrainz:artist:'
-        else:
-            return 'notfound'
-            
-    def __getMB_album_id(self, mb_artist_id, album_name):
-        ''' Queries Headphone's API Musicbrainz query method to get Musicbrainz album id.
-        Returns Musicbrainz album id.
-        if unable to acquire, returns string 'notfound'.
-        '''
-        req = {'cmd': 'findAlbum', 'name': album_name, 'limit': 15}
-        count = 0
-        while True: # retry connection if failed, until successful or 5 tries
-            count += 1
-            albumQuery = self.__callHeadphones(req)
-            if isinstance(albumQuery, list):
-                break
-            if count > 5:
-                mb_album_id = "notfound"
-                break
-        if mb_album_id != "notfound":
-            for album in albumQuery:
-               if album['id'] == mb_artist_id and (album['title']).lower() == (album_name).lower():  #ignore case
-                   mb_album_id = album['rgid']  #Headphones prefers the release group id
-                   break
-               else:
-                   mb_album_id = "notfound"
-        return mb_album_id
-    
-    def __callHeadphones(req):
-        #TODO find a way to pass config maybe into constructor
-        data = { 'apikey': ConfigSectionMap("HEADPHONES")['api_key'] }
-        payload = {}
-        for item in (data, req):
-            payload.update(item)
-        result = requests.get(hp_api, params=payload)
-        result = result.json()
-        return result
+        self.dl_request_status  = None
