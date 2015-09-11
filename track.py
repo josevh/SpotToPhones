417403508a95c403ce6b0677c90e68c776205ec4
Jose Herrera <josevictorherrera@gmail.com>
2015-09-11 17:45:25 +0000

    add method to get mb_album_id

--- track.py
+++ track.py
@@ -1,4 +1,5 @@
 import pyen
+import requests
 
 class Track(object):
     '''Tracks have the following attributes:
@@ -34,22 +35,22 @@ class Track(object):
         
         self.config             = config
         
-        self.mb_artist_id       = self._get_mb_artist_id(self.sp_artist_id)
-        self.mb_id              = None
-        self.mb_album_id        = None
+        self.mb_artist_id       = self._get_mb_artist_id()
+        #self.mb_id              = None
+        self.mb_album_id        = self._get_mb_album_id()
         
         self.have_track         = None
         self.have_album         = None
         
         self.dl_request_status  = None
         
-    def _get_mb_artist_id(self, sp_artist_id):
+    def _get_mb_artist_id(self):
         ''' Use EchoNest API to map Spotify artist id to Musicbrainz artist id
         '''
         config = self.config
         en = pyen.Pyen(config.get('ECHONEST', 'api_key'))
         params = {
-            'id':       'spotify:artist:' + sp_artist_id,
+            'id':       'spotify:artist:' + self.sp_artist_id,
             'bucket':   ['id:musicbrainz'],
         }
         response = en.get('artist/profile', **params)
@@ -59,3 +60,38 @@ class Track(object):
         else:
             return 'notfound'
 
+    def _get_mb_album_id(self):
+        ''' Queries Headphone's API Musicbrainz query method to get Musicbrainz album id.
+            Returns Musicbrainz album id.
+            if unable to acquire, returns string 'notfound'.
+        '''
+        req = {'cmd': 'findAlbum', 'name': self.name, 'limit': 15}
+        count = 0
+        hp_album_id = None
+        while True: # retry connection if failed, until successful or 5 tries
+            count += 1
+            albumQuery = self.__callHeadphones(req)
+            if isinstance(albumQuery, list):
+                break
+            if count > 5:
+                mb_album_id = "notfound"
+                break
+        if hp_album_id != "notfound":
+            for album in albumQuery:
+              if album['id'] == self.mb_artist_id and (album['title']).lower() == (self.name).lower():  #ignore case
+                  mb_album_id = album['rgid']  #Headphones prefers the release group id
+                  break
+              else:
+                  mb_album_id = "notfound"
+        return mb_album_id
+    
+    def __callHeadphones(self, req):
+        config = self.config
+        data = { 'apikey': config.get('HEADPHONES', 'api_key') }
+        payload = {}
+        for item in (data, req):
+            payload.update(item)
+        hp_api = "http://" + config.get('HEADPHONES', 'ip') + ":" + config.get('HEADPHONES', 'port') +  config.get('HEADPHONES', 'webroot') + "/api"
+        result = requests.get(hp_api, params=payload)
+        result = result.json()
+        return result
