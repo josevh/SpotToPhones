4987a2ab3b426e6d937192c80c48cbc74b25b528
Jose V Herrera <josevictorherrera@gmail.com>
2015-12-11 15:50:51 +0000

    use Spotify class

--- SpotToPhones.py
+++ SpotToPhones.py
@@ -1,9 +1,14 @@
 import ConfigParser
+from Spotify import Spotify
+
+import pprint #TODO: deleteme
 
 def main():
-    pass
+    spot = Spotify(config)
+    pls = spot.get_playlists(config.get('SPOTIPY', 'user'))
+    pp.pprint(pls)
             
-    
+pp = pprint.PrettyPrinter(indent=4) #TODO: deleteme    
 config = ConfigParser.ConfigParser()
 config.read("config.ini")
 
