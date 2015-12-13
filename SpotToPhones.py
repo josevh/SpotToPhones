970014ce9a4221150b92a2223d1b7eb61d4639da
Jose V Herrera <josevictorherrera@gmail.com>
2015-12-13 04:28:20 +0000

    pretty main

--- SpotToPhones.py
+++ SpotToPhones.py
@@ -1,26 +1,9 @@
 import ConfigParser
 from Spotify import Spotify
 
-import pprint #TODO: deleteme
-
 def main():
+    config = ConfigParser.ConfigParser()
+    config.read("config.ini")
     spot = Spotify(config)
-    #playlists = spot.get_playlists()
-    #playlist_id = spot.get_playlist_id(config.get('SPOTIFY', 'wanted_playlist')) #2WgSH6ivNs8bixraYXBf8C
-    #tracks = spot.get_playlist_tracks('2WgSH6ivNs8bixraYXBf8C') #3CKCZ9pfwAfoMZlMncA1Nc #adele 21 set-fire-to-the-rain
-    
-    # [0]['track']['id']
-    # [0]['track']['name']
-    # [0]['track']['artists'][0]['name']
-    # [0]['track']['artists'][0]['id']
-    # [0]['track']['album']['id']
-    # [0]['track']['album']['name']
-    
-    #pp.pprint(tracks)
-    
-            
-pp = pprint.PrettyPrinter(indent=4) #TODO: deleteme    
-config = ConfigParser.ConfigParser()
-config.read("config.ini")
 
-main()
+if __name__ == "__main__": main()
